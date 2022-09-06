d82f89537f2af012c5535512da318a2c7238e18a
Vladimirrom <vladimirrom0707@gmail.com>
2022-09-06 11:14:55 +0300

    modified class Pi {override toString method}

--- src/com/mycompany/utility/Pi.java
+++ src/com/mycompany/utility/Pi.java
@@ -2,4 +2,9 @@ package com.mycompany.utility;
 
 public class Pi {
     public static final double PI = 3.14;
+
+    @Override
+    public String toString() {
+        return getClass().getName() + ": PI=" + PI;
+    }
 }
